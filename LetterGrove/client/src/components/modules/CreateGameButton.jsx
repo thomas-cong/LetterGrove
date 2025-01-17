94047a2dac4777ef3e1d35dd8c59f8016719c9fd
Leon Chen <leonchen@dhcp-10-29-148-13.dyn.mit.edu>
2025-01-16 23:09:28 -0500

    fixed the create lobby sign

--- LetterGrove/client/src/components/modules/CreateGameButton.jsx
+++ LetterGrove/client/src/components/modules/CreateGameButton.jsx
@@ -44,8 +44,8 @@ const CreateGameButton = () => {
   return (
     <div>
       {!LobbyShowing && (
-        <div onClick={showLobby}>
-          <img src={buttonImage} className="sign" />
+        <div onClick={showLobby} className="button-container">
+          <img src={buttonImage} className="sign" alt="Wooden Sign" />
           <h2 className="text">Create Lobby</h2>
         </div>
       )}
