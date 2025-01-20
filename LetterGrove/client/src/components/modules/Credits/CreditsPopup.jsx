7d80b0e66beafac98ccbff5834b2cd4b21d815da
Leon Chen <leonchen@Leons-MacBook-Pro.local>
2025-01-20 15:48:24 -0500

    changed font and fixed up the create lobby screen

--- LetterGrove/client/src/components/modules/Credits/CreditsPopup.jsx
+++ LetterGrove/client/src/components/modules/Credits/CreditsPopup.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Closebutton from "../../../assets/Closebutton.png";
+import "./CreditsPopup.css";
+
 const CreditsPopup = (props) => {
   return (
     <div className="creditsmainboard">
@@ -16,10 +18,12 @@ const CreditsPopup = (props) => {
             artwork and music. Artistically, we hope that the game is not only fun, but also
             visually attractive and cohesive.
           </div>
+          <h3>
           <div style={{ color: "rgb(94, 129, 255)" }}>
             All artwork, including the logo and game pieces, was handdrawn by Thomas Cong over the
             course of Weblab 2025. All music was composed by Thomas Cong.
           </div>
+          </h3>
           <div style={{ color: "rgb(94, 129, 255)" }}>
             Game logic was implemented by Steve Zhang, and frontend development was completed by
             Leon Chen and Thomas Cong.
