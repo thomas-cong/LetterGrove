2a7be2c66ca3a91d4a97ac38a06523e2cd5bdb05
Thomas Cong <tcong@dhcp-10-29-254-226.dyn.MIT.EDU>
2025-01-23 15:19:25 -0500

    Set up player display rendering for username based off of user id

---
+++ client/src/components/modules/GameComponent/Rankings.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PlayerDisplay from "../../modules/PlayerDisplay";
+import testProfilePicture from "../../../assets/TestingPFP.png";
+import "./Rankings.css";
+
+/**
+ * Rankings component displays the current rankings of players in the game
+ *
+ * @param {Object} props
+ * @param {Array} props.rankings - Array of player rankings with usernames and scores
+ * @param {string} props.currentUserId - Current user's ID
+ */
+const Rankings = ({ rankings = [], currentUserId }) => {
+  return (
+    <div className="rankings-container">
+      <h3>Rankings</h3>
+      {rankings &&
+        rankings.map((player, index) => (
+          <PlayerDisplay
+            key={index}
+            name={`${player.username} - ${player.score} pts`}
+            profilePicture={testProfilePicture}
+            playerId={player.playerId}
+            currentUserId={currentUserId}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Rankings;
--- client/src/components/modules/PlayerDisplay.jsx
+++ client/src/components/modules/PlayerDisplay.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "./PlayerDisplay.css";
 
-// @param {name} name of the player
-// @param {profile picture} profile picture of the player
+/**
+ * Component to display a player's information
+ * @param {string} props.name - Player's name
+ * @param {string} props.profilePicture - URL of the player's profile picture
+ * @param {string} props.playerId - Player's ID
+ * @param {string} props.currentUserId - Current user's ID
+ */
 const PlayerDisplay = (props) => {
+  const isCurrentUser = props.playerId === props.currentUserId;
+  const className = `playerDisplay ${isCurrentUser ? "current-user" : ""}`.trim();
+  
   return (
-    <div className="playerDisplay">
+    <div className={className}>
       <img src={props.profilePicture} alt="Profile Picture" className="profilePicture" />
       <p className="playerName">{props.name}</p>
     </div>
   );
 };
+
 export default PlayerDisplay;
