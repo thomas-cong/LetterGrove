daebd99c33c6f801b5257d973c56b7808d5b9449
Thomas Cong <tcong@dhcp-10-29-254-226.dyn.MIT.EDU>
2025-01-25 02:37:44 -0500

    set up score counting vanity :)

--- client/src/components/modules/GameComponent/PointsCounter.jsx
+++ client/src/components/modules/GameComponent/PointsCounter.jsx
@@ -1,14 +1,41 @@
 import React, { useState, useEffect } from "react";
-import { socket } from "../../../client-socket.js";
+import ScoreBox from "../../../assets/ScoreBox.png";
+import "./PointsCounter.css";
 
-// params.points is the current points
-// Simply render them (TODO: create image to go behind the text)
+const COLORS = ["#75c44c", "#194bb7", "#ea2f2f"];
+
 const PointsCounter = (params) => {
+  const [digitColors, setDigitColors] = useState([COLORS[0], COLORS[0], COLORS[0]]);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const formattedPoints = params.points.toString().padStart(3, "0");
+
+  useEffect(() => {
+    setDigitColors(
+      Array(3)
+        .fill()
+        .map(() => COLORS[Math.floor(Math.random() * COLORS.length)])
+    );
+    setIsAnimating(true);
+    const timer = setTimeout(() => setIsAnimating(false), 300);
+    return () => clearTimeout(timer);
+  }, [params.points]);
+
   return (
-    <div>
-      <span>Points:</span>
-      <span>{params.points}</span>
+    <div className="points-counter">
+      <img src={ScoreBox} alt="ScoreBox" />
+      <span>
+        {formattedPoints.split("").map((digit, index) => (
+          <span
+            key={index}
+            style={{ color: digitColors[index] }}
+            className={isAnimating ? "score-change" : ""}
+          >
+            {digit}
+          </span>
+        ))}
+      </span>
     </div>
   );
 };
+
 export default PointsCounter;
