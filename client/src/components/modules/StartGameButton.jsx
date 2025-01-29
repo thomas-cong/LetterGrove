d58a041375202622cf537f1b79d41205d02e4b20
Thomas Cong <tcong@dhcp-10-29-254-226.dyn.MIT.EDU>
2025-01-29 09:13:28 -0500

    Added alert when user logs out in middle of lobby

--- client/src/components/modules/StartGameButton.jsx
+++ client/src/components/modules/StartGameButton.jsx
@@ -1,26 +1,34 @@
 import React, { useState, useEffect } from "react";
 import shortSign from "../../assets/320signs_2.png";
 import { post } from "../../utilities";
-import { socket } from "../../client-socket";
+import AlertBox from "./AlertBox/AlertBox.jsx";
 import "./StartGameButton.css";
 
 const StartGameButton = (props) => {
   const [buttonClicked, setButtonClicked] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
   const handleClick = () => {
     // Only send the start game request, don't set animation here
     if (buttonClicked) return;
     console.log("made it here");
     setButtonClicked(true);
-    post("/api/lobbyToGameTransition", { lobbyCode: props.lobbyCode });
+    post("/api/lobbyToGameTransition", { lobbyCode: props.lobbyCode }).catch((error) => {
+      setShowAlert(true);
+      setAlertMessage("Please refresh the page: user not authenticated");
+    });
   };
 
   return (
-    <div className="start-game-button">
-      <div className="sign-container" onClick={handleClick}>
-        <img src={shortSign} className="sign" alt="Wooden Sign" />
-        <h2 className="sign-text">Start Game</h2>
+    <>
+      <div className="start-game-button">
+        <div className="sign-container" onClick={handleClick}>
+          <img src={shortSign} className="sign" alt="Wooden Sign" />
+          <h2 className="sign-text">Start Game</h2>
+        </div>
       </div>
-    </div>
+      {showAlert && <AlertBox message={alertMessage} timeout={3500} setShowAlert={setShowAlert} />}
+    </>
   );
 };
 
